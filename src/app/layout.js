a1945525568ffd5a8c052d7bd53428977ae592d0
Oct - test <oct@email.com>
2025-06-13 20:05:34 +0530

    this will be stashed

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Roboto } from "next/font/google";
 import "./globals.css";
-
+import { Header, Footer } from "@/components/layout";
 const roboto = Roboto({
   variable: "--font-roboto",
   subsets: ["latin"],
@@ -16,7 +16,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${roboto.variable} antialiased`}>
-        <main className="max-w-4xl mx-auto p-4">{children}</main>
+        <main className="max-w-4xl mx-auto p-4">
+          <Header />
+          {children}
+          <Footer />
+        </main>
       </body>
     </html>
   );
